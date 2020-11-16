aa8b69727d9c70e70311c44f9275df9cc0e40f25
Pilkowski <ilsihaterhd@gmail.com>
2020-11-16 19:34:13 +0100

    Rozpoczęto grę, utworzono skrypty dla poruszania się, skakania, kucania oraz zaimplementowano wstępne skrypty dla wrogów i strzałów

---
+++ Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Controller controller;
+
+    public float runSpeed = 3f;
+
+    float horizontalMove = 0;
+
+    bool jump = false;
+    bool crouch = false;
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("update");
+            jump = true;
+        }
+        if(Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
